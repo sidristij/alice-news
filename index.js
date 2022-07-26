a900760bcc0ec508b5e09a7f953e5938eaa334cf
Stanislav Sidristij <1454688+sidristij@users.noreply.github.com>
2022-07-26 22:19:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Home route
 app.post("/", (req, res) => {
-  res.send("{ text: 'Как дела?',  tts: 'Как дел+а?',  buttons: [    {title: 'Отлично', hide: true},    {title: 'Супер', hide: true},  ],  end_session: false}");
+  res.send("{ response: { text: 'Как дела?',  tts: 'Как дел+а?',  buttons: [    {title: 'Отлично', hide: true},    {title: 'Супер', hide: true},  ],  end_session: false} }");
 });
 
 // Mock API
