d84f005e103ddbcde67a39391461a87190f15e2d
Stanislav Sidristij <1454688+sidristij@users.noreply.github.com>
2022-07-26 22:25:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Home route
 app.post("/", (req, res) => {
-  res.send("{ response: { text: 'Как дела?',  tts: 'Как дел+а?',  buttons: [    {title: 'Отлично', hide: true},    {title: 'Супер', hide: true},  ],  end_session: false} }");
+  res.send("{ 'response': { 'text': 'Маамаа! Шикардос амигос!',  'tts': 'Маамаа! Шикард+ос ам+игос!' } }");
 });
 
 // Mock API
@@ -27,6 +27,5 @@ app.post("/user", (req, res) => {
 
 // Listen on port 5000
 app.listen(port, () => {
-  console.log(`Server is booming on port 5000
-Visit http://localhost:5000`);
+  console.log(`Server is booming`);
 });
